c247fc756a89b9e173cb11892d3d72f5db51313c
Boone Epstein <booneepstein@gmail.com>
2021-02-23 17:46:56 -0600

    Add state to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,17 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [locations, setLocations] = useState([]);
+  const [locationInfo, setLocationInfo] = useState({
+    city: '',
+    state: '',
+    country: '',
+    current: {},
+    forcast: [],
+    history: []
+
+  });
+
   return (
     <div className="App">
       PLACEHOLDER
