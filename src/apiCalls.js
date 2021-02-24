451308b57414007c1639bc74902229b053b3d97b
Boone Epstein <booneepstein@gmail.com>
2021-02-24 16:59:38 -0600

    Change api key

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,18 +1,18 @@
 const apiCalls = {
   getStates() {
-    return fetch('http://api.airvisual.com/v2/states?country=USA&key=568b7a67-472c-4e0a-8818-ca1055fc0494')
+    return fetch('http://api.airvisual.com/v2/states?country=USA&key=6c462cce-4580-4d36-b154-0e6f353635c6')
       .then(response => response.json())
       .then(states => states.data.map(state => state.state))
   },
 
   getCities(state) {
-    return fetch(`http://api.airvisual.com/v2/cities?state=${state}&country=USA&key=568b7a67-472c-4e0a-8818-ca1055fc0494`)
+    return fetch(`http://api.airvisual.com/v2/cities?state=${state}&country=USA&key=6c462cce-4580-4d36-b154-0e6f353635c6`)
       .then(response => response.json())
       .then(cities => cities.data.map(city => city.city))
   },
 
   getLocationData(state, city) {
-    return fetch(`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=568b7a67-472c-4e0a-8818-ca1055fc0494`)
+    return fetch(`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=6c462cce-4580-4d36-b154-0e6f353635c6`)
       .then(response => response.json())
       .then(data => data.data)
   }
