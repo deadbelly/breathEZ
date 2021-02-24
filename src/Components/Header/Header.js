e82d80cbde60ea2c3e99878aa67628cbd7505962
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-24 11:31:14 -0700

    Create basic header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-
-}
+const Header = () => (
+  <div>
+    <h1>BreathEZ</h1>
+    <Link to='/faq'>FAQ</Link>
+    // this will be where the Search functionality will go
+  </div>
+)
 
 export default Header;
