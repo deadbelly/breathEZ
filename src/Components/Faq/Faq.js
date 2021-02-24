5c5fd0c3537cffdf82b2bb07a6a61f1672454344
Thao Ma <MaThao@gmail.com>
2021-02-23 19:42:07 -0700

    Add what is AQI section

--- src/Components/Faq/Faq.js
+++ src/Components/Faq/Faq.js
@@ -2,7 +2,20 @@ import React from 'react';
 import './Faq.css';
 
 const Faq = () => {
+  return (
+    <section>
+      <h1>FAQ</h1>
+      <h2>What is AQI</h2>
+      <p>
+        Air Quality Index(AQI) is a tool for the United States Enviromental Protection Agency(EPA). AQI uses color-coded categories and provides statements for each category that tell you about air quality in your area, which groups of people may be affected, and steps you can take to reduce your exposure to air pollution and is a basis for reporting current air quality.
+      </p>
+      <h2>How does AQI work?</h2>
+      <p>
+        
+      </p>
 
+    </section>
+  )
 }
 
 export default Faq;
