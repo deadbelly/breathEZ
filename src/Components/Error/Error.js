a30d83108f976c77a9d052f51c3819f245420e66
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-24 10:19:00 -0700

    Add conditional rendering based on err msg

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Error.css'
 
-const Error = () => (
+const Error = errDets => (
   <div className='error'>
     <h1>Sorry!</h1>
-    <h2>Something went wrong.</h2>
+    {errDets.status && <h2>{errDets.status}</h2>}
+    {errDets.status.toString()[0] === 4 && <h2>Something went wrong!</h2>}
+    {errDets.status.toString()[0] === 5 && <h2>Something went wrong with the server!</h2>}
     <h3>Please try again later!</h3>
   </div>
 )
