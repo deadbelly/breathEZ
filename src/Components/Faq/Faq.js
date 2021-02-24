bec3693941faee7be135446af46ab411abd16d52
Thao Ma <MaThao@gmail.com>
2021-02-23 20:10:03 -0700

    Add How does AQI work section and an assets folder

--- src/Components/Faq/Faq.js
+++ src/Components/Faq/Faq.js
@@ -11,7 +11,14 @@ const Faq = () => {
       </p>
       <h2>How does AQI work?</h2>
       <p>
-        
+        AQI goes from 0 to 500. The higher the AQI value, the greater level of air pollution and health concerns. For example, an AQI value of 50 or below represents good air quality, while an AQI value over 300 represents hazardous air quality.
+      </p>
+      <p>
+        For each pollutant an AQI value of 100 generally corresponds to an ambient air concentration that equals the level of the short-term national ambient air quality standard for protection of public health. AQI values at or below 100 are generally thought of as satisfactory. When AQI values are above 100, air quality is unhealthy: at first for certain sensitive groups of people, then for everyone as AQI values get higher.
+      </p>
+      <p>
+        The AQI is divided into six categories. Each category corresponds to a different level of health concern. Each category also has a specific color. The color makes it easy for people to quickly determine whether air quality is reaching unhealthy levels in their communities.
+        <img src='' />
       </p>
 
     </section>
