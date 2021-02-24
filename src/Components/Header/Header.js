8793acaac010e6735941670d074d0f95fdd0255a
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-23 19:04:58 -0700

    Import Faq and SearchBar to Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Header.css';
 
+import Faq from '../Faq/Faq.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+
 const Header = () => (
   <div>
     <h1>BreathEZ</h1>
