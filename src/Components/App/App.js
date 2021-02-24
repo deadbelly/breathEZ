059db92a798fd2650150f2f398914ce8897b822e
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-24 16:44:09 -0700

    Remove useEffect as App is not using

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import './App.css';
