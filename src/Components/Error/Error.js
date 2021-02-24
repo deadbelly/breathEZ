3e1a2b1bc914290b1b0abf63ceb952cc2ddb5289
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-23 19:55:35 -0700

    Add main body of Error

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Error.css'
 
-const Error = () => {
-
-}
+const Error = () => (
+  <div>
+    <h1>Sorry!</h1>
+    <h2>Something went wrong.</h2>
+    <h3>Please try again later!</h3>
+  </div>
+)
 
 export default Error;
