006d1c235a2dc4b066e064491a20e6ed3ee0b802
Boone Epstein <booneepstein@gmail.com>
2021-02-25 13:00:54 -0600

    Add rating functions and test calls

--- src/Components/LocationData/LocationData.js
+++ src/Components/LocationData/LocationData.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './LocationData.css'
+import { celsiusToFahr, mpsToMph, aqiRating } from '../../statConversions';
 
 const LocationData = ({data}) => {
-  
+  const rating = aqiRating(data.current.pollution.aqius)
 
   return (
     <div>
       <h1>{data.city}, {data.state}</h1>
-
+      <h1 className={rating.toLowerCase()}>AQI {data.current.pollution.aqius} - {rating}</h1>
     </div>
   )
 }
--- src/statConversions.js
+++ src/statConversions.js
@@ -1,9 +1,29 @@
 const statConversions = {
   celsiusToFahr(degreesC) {
-    return Math.round(degreesC * 1.8 + 32)
+    return Math.round(degreesC * 1.8 + 32);
+  },
+
+  mpsToMph(mps) {
+    return (mps*2.237).toFixed(2);
   },
 
   aqiRating(aqi) {
-    
+    if (aqi < 50) {
+      return 'GOOD';
+    } else if (aqi < 100) {
+      return 'MODERATE';
+    } else if (aqi < 150){
+      return 'RISKY';
+    } else if (aqi < 200) {
+      return 'UNHEALTHY';
+    } else if (aqi < 300) {
+      return 'VERY UNHEALTHY';
+    } else {
+      return 'HAZARDOUS';
+    }
   }
 }
+
+const { celsiusToFahr, mpsToMph, aqiRating } = statConversions;
+
+export { celsiusToFahr, mpsToMph, aqiRating };
