9a9c79df6baaf49eae3e6761aad6a269a850e06e
Thao Ma <MaThao@gmail.com>
2021-02-25 16:01:49 -0700

    Change selector from css to scss

--- src/Components/Selector/Selector.js
+++ src/Components/Selector/Selector.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './Selector.css';
 import { getStates, getCities } from '../../apiCalls';
 
 const Selector = ({getAndSetLocationData}) => {
