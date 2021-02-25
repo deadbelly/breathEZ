118eab67430a230936044a78db3f3cced57cec3c
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-25 13:29:45 -0700

    Set up fixture for Colorado and Denver

--- cypress/integration/main-view-spec.js
+++ cypress/integration/main-view-spec.js
@@ -12,4 +12,19 @@ describe('the main view', () => {
     cy
       .visit('http://localhost:3000');
   });
+
+  it('should show Colorado and Idaho as states', () => {
+  });
+
+  it('should be able to select Denver when Colorado is selected', () => {
+    cy
+      .fixture('../fixtures/coCities.json')
+      .then(data => {
+        cy.intercept('GET', 'http://api.airvisual.com/v2/cities?state=Colorado&country=USA&key=6c462cce-4580-4d36-b154-0e6f353635c6', {
+          statusCode: 200,
+          body: data
+        });
+      });
+
+  });
 });
