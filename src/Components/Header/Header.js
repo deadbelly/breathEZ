e8c385db122fbcb709c4674bd795a19244a50313
Thao Ma <MaThao@gmail.com>
2021-02-24 18:37:32 -0700

    Change index.css and header.css to .scss

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,11 +3,11 @@ import './Header.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <div>
+  <header>
     <h1>BreathEZ</h1>
     <Link to='/faq'>FAQ</Link>
     // this will be where the Search functionality will go
-  </div>
+  </header>
 )
 
 export default Header;
