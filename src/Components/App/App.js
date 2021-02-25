4d1aaa61da2fad893511180255501875dcd3f5b7
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-24 18:24:38 -0700

    Render different links depending on url

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,15 +4,10 @@ import Header from '../Header/Header';
 import './App.css';
 import { getLocationData } from '../../apiCalls';
 import Selector from '../Selector/Selector';
-import Faq from '../Faq/Faq'
+import Faq from '../Faq/Faq';
 
 const App = () => {
-  const [faqDisplay, setFaqDisplay] = useState(false);
   const [locationData, setLocationData] = useState(null);
-
-  const toggleFaq = () => {
-    setFaqDisplay(!faqDisplay)
-  }
 
   const getAndSetLocationData = async (state, city) => {
     setLocationData(await getLocationData(state, city))
@@ -20,20 +15,30 @@ const App = () => {
 
   return (
     <div className="App">
-      <Header toggleFaq={toggleFaq}/>
-      <Route
-        exact path='/'
-        render={() => (
-          <Selector 
-            getAndSetLocationData={getAndSetLocationData}
-          />
-        )} 
-      />
       <Route
         exact path='/faq'
-        render={() => (
-          <Faq /> 
-        )} 
+        render={() => {
+          return (
+            <div>
+              <Header />
+              <Faq /> 
+            </div>
+          )
+        }} 
+      />
+      <Route
+        exact path='/'
+        render={props => {
+          const path = props.match.path;
+          return (
+            <div>
+              <Header path={path}/>
+              <Selector 
+                getAndSetLocationData={getAndSetLocationData}
+              />
+            </div>
+          )
+        }} 
       />
     </div>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
+const Header = ({path}) => (
   <div>
     <h1>BreathEZ</h1>
-    <Link to='/faq'>FAQ</Link>
+    {path === '/' ? <Link to='/faq'>FAQ</Link> : <Link to='/'>Back to Home</Link>}
   </div>
-)
+);
+
 
 export default Header;
