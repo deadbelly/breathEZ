4498340bdd1ac02f36c4422d123bc378278c28ad
Boone Epstein <booneepstein@gmail.com>
2021-02-25 14:16:21 -0600

    Move Assets

--- src/Components/LocationData/LocationData.js
+++ src/Components/LocationData/LocationData.js
@@ -4,11 +4,14 @@ import { celsiusToFahr, mpsToMph, aqiRating } from '../../statConversions';
 
 const LocationData = ({data}) => {
   const rating = aqiRating(data.current.pollution.aqius)
+  const src = `../../assets/icons/${data.current.weather.ic}.png`
+  console.log(data.current.weather.ic)
 
   return (
     <div>
       <h1>{data.city}, {data.state}</h1>
       <h1 className={rating.toLowerCase()}>AQI {data.current.pollution.aqius} - {rating}</h1>
+      <img src={src} />
     </div>
   )
 }
