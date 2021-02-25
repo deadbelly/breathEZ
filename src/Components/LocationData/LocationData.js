809e4d0a1e176f421afe55d5d8d5158e5cb32557
Boone Epstein <booneepstein@gmail.com>
2021-02-25 15:26:09 -0600

    Dynamically import file paths

--- src/Components/LocationData/LocationData.js
+++ src/Components/LocationData/LocationData.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './LocationData.css'
 import { celsiusToFahr, mpsToMph, aqiRating } from '../../statConversions';
+import iconPaths from '../../iconPaths'
 
 const LocationData = ({data}) => {
   const rating = aqiRating(data.current.pollution.aqius)
-  const src = `../../assets/icons/${data.current.weather.ic}.png`
   console.log(data.current.weather.ic)
 
+  const src = iconPaths[data.current.weather.ic]
   return (
-    <div>
+    <div className='locationData'>
       <h1>{data.city}, {data.state}</h1>
       <h1 className={rating.toLowerCase()}>AQI {data.current.pollution.aqius} - {rating}</h1>
       <img src={src} />
+      <h2>WEATHER</h2>
+      <h3>Temperature: {celsiusToFahr(data.current.weather.tp)}˚F</h3>
+      <h3>Wind Speed: {mpsToMph(data.current.weather.ws)}mph</h3>
+      <h3>Humidity: {data.current.weather.hu}%</h3>
     </div>
   )
 }
---
+++ src/iconPaths.js
@@ -0,0 +1,41 @@
+import clearSkyD from './assets/icons/01d.png';
+import clearSkyN from './assets/icons/01n.png';
+import fewCloudsD from './assets/icons/02d.png';
+import fewCloudsN from './assets/icons/02n.png';
+import scatteredClouds from './assets/icons/03d.png';
+import brokenClouds from './assets/icons/04d.png';
+import showerRain from './assets/icons/09d.png';
+import rainD from './assets/icons/10d.png';
+import rainN from './assets/icons/10n.png';
+import thunderstorm from './assets/icons/11d.png';
+import snow from './assets/icons/13d.png';
+import mist from './assets/icons/50d.png';
+
+
+const iconPaths = {
+'01d': clearSkyD,
+
+'01n': clearSkyN,
+
+'02d': fewCloudsD,
+
+'02n': fewCloudsN,
+
+'03d': scatteredClouds,
+
+'04d': brokenClouds,
+
+'09d': showerRain,
+
+'10d': rainD,
+
+'10n': rainN,
+
+'11d': thunderstorm,
+
+'13d': snow,
+
+'50d': mist
+};
+
+export default iconPaths;
