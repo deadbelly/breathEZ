58ee1fe0f86a74fec083f10d3930b25f1e538d3d
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-26 21:19:36 -0700

    Move test return to home page to faq tests

---
+++ cypress/integration/faq-spec.js
@@ -0,0 +1,49 @@
+describe('the faq page', () => {
+  beforeEach(() => {
+    cy
+      .fixture('../fixtures/statesData.json')
+      .then(data => {
+        cy.intercept('GET', 'http://api.airvisual.com/v2/states?country=USA&key=6c462cce-4580-4d36-b154-0e6f353635c6', {
+          statusCode: 200,
+          body: data
+        });
+      });
+
+    cy
+      .fixture('../fixtures/idCities.json')
+      .then(data => {
+        cy.intercept('GET', 'http://api.airvisual.com/v2/cities?state=Idaho&country=USA&key=6c462cce-4580-4d36-b154-0e6f353635c6', {
+          statusCode: 200,
+          body: data
+        });
+      });
+
+    cy
+      .fixture('../fixtures/coCities.json')
+      .then(data => {
+        cy.intercept('GET', 'http://api.airvisual.com/v2/cities?state=Colorado&country=USA&key=6c462cce-4580-4d36-b154-0e6f353635c6', {
+          statusCode: 200,
+          body: data
+        });
+      });
+
+    cy
+      .visit('http://localhost:3000');
+  });
+
+  it('should be able to return to the home page', () => {
+    cy
+      .get('a')
+      .click()
+
+      .get('img')
+      .should('have.attr', 'src', '/static/media/AQI-chart.7b1ed9f0.jpg')
+
+    cy
+      .get('a')
+      .click()
+
+      .get('form')
+      .should('exist')
+  });
+});
