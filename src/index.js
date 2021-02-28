bfa94702d6ee83d169a0c9ee5e6d605d408047f1
Boone Epstein <booneepstein@gmail.com>
2021-02-28 13:33:49 -0600

    Remove unused worker

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './Components/App/App';
 import { BrowserRouter } from 'react-router-dom'
-import * as serviceWorker from './serviceWorker';
 
 const router = <BrowserRouter> <App /> </BrowserRouter>;
 
 ReactDOM.render(router, document.getElementById('root'));
-
-// serviceWorker.register();
