e5dd92af1a61d7410718c7445ddc32ed58816323
Thao Ma <MaThao@gmail.com>
2021-02-28 16:32:06 -0700

    Add desktop-view for locationData component

--- src/Components/LocationData/LocationData.js
+++ src/Components/LocationData/LocationData.js
@@ -8,15 +8,21 @@ const LocationData = ({data}) => {
   const src = iconPaths[data.current.weather.ic]
   return (
     <div className='locationData'>
-      <h1>{data.city}, {data.state}</h1>
-      <h1 
-        className='aqi-score'>AQI {data.current.pollution.aqius} -
-        <span className={rating.toLowerCase()}> {rating}</span>
-      </h1>
-      <img className='weather-icon' src={src} alt='weather icon' />
-      <h2>Temperature: <b className='location-bold'>{celsiusToFahr(data.current.weather.tp)}˚F</b></h2>
-      <h2>Wind Speed: <b className='location-bold'>{mpsToMph(data.current.weather.ws)}mph</b></h2>
-      <h2>Humidity: <b className='location-bold'>{data.current.weather.hu}%</b></h2>
+      <div className='wrapper'>
+        <h1>{data.city}, {data.state}</h1>
+        <h1 
+          className='aqi-score'>AQI {data.current.pollution.aqius} -
+          <span className={rating.toLowerCase()}> {rating}</span>
+        </h1>
+      </div>
+      <div>
+        <img className='weather-icon' src={src} alt='weather icon' />
+      </div>
+      <div className='wrapper'>
+        <h2>Temperature: <b className='location-bold'>{celsiusToFahr(data.current.weather.tp)}˚F</b></h2>
+        <h2>Wind Speed: <b className='location-bold'>{mpsToMph(data.current.weather.ws)}mph</b></h2>
+        <h2>Humidity: <b className='location-bold'>{data.current.weather.hu}%</b></h2>
+      </div>
     </div>
   )
 }
