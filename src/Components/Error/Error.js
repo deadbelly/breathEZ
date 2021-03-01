e7684e46e6e5230e58b87ce34c67f60c4548530e
Boone Epstein <booneepstein@gmail.com>
2021-03-01 17:27:32 -0600

    Add console log to error component

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Error = ({error}) => (
-  <div className='error'>
-    <h1>{error.status}</h1>
-    <h2>{error.statusText}</h2>
-  </div>
-)
+const Error = ({error}) => {
+  console.log('ERROR', error)
+  return (
+    <div className='error'>
+      <h1>{error.status}</h1>
+      <h2>{error.statusText}</h2>
+    </div>
+  )
+}
 
 export default Error;
