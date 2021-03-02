ad06dc12ce2fbe9d6fb3d297a63ce7b2a8ddb4a0
Boone Epstein <booneepstein@gmail.com>
2021-03-01 18:39:26 -0600

    Reset API key

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,25 +8,25 @@ const apiCalls = {
   },
 
   getStates() {
-    return fetch('http://api.airvisual.com/v2/states?country=USA&key=8b060d59-0210-4921-886d-d89f44fa1739')
+    return fetch('http://api.airvisual.com/v2/states?country=USA&key=13480d37-c37a-4013-aef2-8826ac3d8390')
       .then(response => apiCalls.checkResponse(response))
       .then(states => states.data.map(state => state.state))
   },
 
   getCities(state) {
-    return fetch(`http://api.airvisual.com/v2/cities?state=${state}&country=USA&key=8b060d59-0210-4921-886d-d89f44fa1739`)
+    return fetch(`http://api.airvisual.com/v2/cities?state=${state}&country=USA&key=13480d37-c37a-4013-aef2-8826ac3d8390`)
       .then(response => apiCalls.checkResponse(response))
       .then(cities => cities.data.map(city => city.city))
   },
 
   getLocationData(state, city) {
-    return fetch(`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=8b060d59-0210-4921-886d-d89f44fa1739`)
+    return fetch(`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=13480d37-c37a-4013-aef2-8826ac3d8390`)
       .then(response => apiCalls.checkResponse(response))
       .then(data => data.data)
   },
 
   getNearestData() {
-    return fetch('http://api.airvisual.com/v2/nearest_city?key=8b060d59-0210-4921-886d-d89f44fa1739')
+    return fetch('http://api.airvisual.com/v2/nearest_city?key=13480d37-c37a-4013-aef2-8826ac3d8390')
       .then(response => apiCalls.checkResponse(response))
       .then(data => data.data)
   }
