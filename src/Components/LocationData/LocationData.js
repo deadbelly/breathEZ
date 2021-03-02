c7302199685ab443caf75cc9aa9c4ddfc06f46b3
Boone Epstein <booneepstein@gmail.com>
2021-03-01 19:14:50 -0600

    Add data cleaning

--- src/Components/LocationData/LocationData.js
+++ src/Components/LocationData/LocationData.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { celsiusToFahr, mpsToMph, aqiRating } from '../../statConversions';
+import { aqiRating } from '../../statConversions';
 import iconPaths from '../../iconPaths'
 
 const LocationData = ({data}) => {
-  const rating = aqiRating(data.current.pollution.aqius)
+  const rating = aqiRating(data.aqi)
 
-  const src = iconPaths[data.current.weather.ic]
+  const src = iconPaths[data.ic]
   return (
     <div className='locationData'>
       <div className='wrapper'>
         <h1>{data.city}, {data.state}</h1>
-        <h1 
-          className='aqi-score'>AQI {data.current.pollution.aqius} -
+        <h1
+          className='aqi-score'>AQI {data.aqi} -
           <span className={rating.toLowerCase()}> {rating}</span>
         </h1>
       </div>
@@ -19,9 +19,9 @@ const LocationData = ({data}) => {
         <img className='weather-icon' src={src} alt='weather icon' />
       </div>
       <div className='wrapper'>
-        <h2>Temperature: <b className='location-bold'>{celsiusToFahr(data.current.weather.tp)}˚F</b></h2>
-        <h2>Wind Speed: <b className='location-bold'>{mpsToMph(data.current.weather.ws)}mph</b></h2>
-        <h2>Humidity: <b className='location-bold'>{data.current.weather.hu}%</b></h2>
+        <h2>Temperature: <b className='location-bold'>{data.tp}˚F</b></h2>
+        <h2>Wind Speed: <b className='location-bold'>{data.ws}mph</b></h2>
+        <h2>Humidity: <b className='location-bold'>{data.hu}%</b></h2>
       </div>
     </div>
   )
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,23 @@
+import { celsiusToFahr, mpsToMph } from './statConversions';
+
 const apiCalls = {
   checkResponse(response) {
     if (response.ok) {
       return response.json()
     } else {
       throw response
+    }
+  },
+
+  cleanData(data){
+    return {
+      state: data.state,
+      city: data.city,
+      aqi: data.current.pollution.aqius,
+      ic: data.current.weather.ic,
+      tp: celsiusToFahr(data.current.weather.tp),
+      ws: mpsToMph(data.current.weather.ws),
+      hu: data.current.weather.hu
     }
   },
 
@@ -22,13 +36,13 @@ const apiCalls = {
   getLocationData(state, city) {
     return fetch(`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=13480d37-c37a-4013-aef2-8826ac3d8390`)
       .then(response => apiCalls.checkResponse(response))
-      .then(data => data.data)
+      .then(data => apiCalls.cleanData(data.data))
   },
 
   getNearestData() {
     return fetch('http://api.airvisual.com/v2/nearest_city?key=13480d37-c37a-4013-aef2-8826ac3d8390')
       .then(response => apiCalls.checkResponse(response))
-      .then(data => data.data)
+      .then(data => apiCalls.cleanData(data.data))
   }
 }
 
