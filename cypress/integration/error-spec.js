e6785d97804348ecddf2b5e83e1b11df04d882bc
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-02 17:19:15 -0700

    Fix 404 spacing

--- cypress/integration/error-spec.js
+++ cypress/integration/error-spec.js
@@ -22,19 +22,19 @@ describe('403 errors', () => {
   });
 });
 
-  describe('404 errors', () => {
-    beforeEach(() => {
-      cy
-        .fixture('../fixtures/statesData.json')
-        .then(data => {
-          cy.intercept('GET', `https://api.airvisual.com/v2/states?country=USA&key=${key}`, {
-            statusCode: 404
-          });
-      });
-
-      cy
-        .visit('http://localhost:3000') 
+describe('404 errors', () => {
+  beforeEach(() => {
+    cy
+      .fixture('../fixtures/statesData.json')
+      .then(data => {
+        cy.intercept('GET', `https://api.airvisual.com/v2/states?country=USA&key=${key}`, {
+          statusCode: 404
+        });
     });
+
+    cy
+      .visit('http://localhost:3000') 
+  });
 
   it('should render', () => {
     cy
