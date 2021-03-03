3e9257477340f2eef5462ef9405a7999b8e20858
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-02 17:18:40 -0700

    Describe 404 errors

--- cypress/integration/error-spec.js
+++ cypress/integration/error-spec.js
@@ -21,3 +21,25 @@ describe('403 errors', () => {
       .should('contain', 'Forbidden')
   });
 });
+
+  describe('404 errors', () => {
+    beforeEach(() => {
+      cy
+        .fixture('../fixtures/statesData.json')
+        .then(data => {
+          cy.intercept('GET', `https://api.airvisual.com/v2/states?country=USA&key=${key}`, {
+            statusCode: 404
+          });
+      });
+
+      cy
+        .visit('http://localhost:3000') 
+    });
+
+  it('should render', () => {
+    cy
+      .get('body')
+      .should('contain', '404')
+      .should('contain', 'Not Found')
+  });
+});
