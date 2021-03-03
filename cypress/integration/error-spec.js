3e3118cb8a57f9db9e414335f166a81f6418ef03
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-02 17:14:45 -0700

    Check for 403 errors

---
+++ cypress/integration/error-spec.js
@@ -0,0 +1,23 @@
+import { key } from '../../src/apiCalls';
+
+describe('403 errors', () => {
+  beforeEach(() => {
+    cy
+      .fixture('../fixtures/statesData.json')
+      .then(data => {
+        cy.intercept('GET', `https://api.airvisual.com/v2/states?country=USA&key=${key}`, {
+          statusCode: 403
+        });
+    });
+
+    cy
+      .visit('http://localhost:3000') 
+  });
+
+  it('should render', () => {
+    cy
+      .get('body')
+      .should('contain', '403')
+      .should('contain', 'Forbidden')
+  });
+});
